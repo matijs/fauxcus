bfa06c077fcf065a51fdc6c7d920bb80b216580e
matijs <matijs@gmail.com>
2018-10-01 20:09:41 +0200

    Merge fauxcus.js and index.js
    Remove unneeded complexity.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-export { fauxcus } from './fauxcus';
+import { blurHandler } from './blur-handler';
+
+export function fauxcus(node) {
+
+  if (!(node instanceof HTMLElement)) throw new Error('Valid node needed');
+
+  if (!node.hasAttribute('tabindex') && node.tabIndex === -1) {
+
+    node.setAttribute('tabindex', '-1');
+    node.addEventListener('blur', blurHandler);
+
+  }
+
+  node.focus();
+
+}
+
